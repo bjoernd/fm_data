9fb7bdda079ea21b9997bfc0d93b6d203aed463c
Bjoern Doebel <bjoern.doebel@gmail.com>
2023-01-04 23:57:56 +0100

    Adjust sheet lookup
    We used the Drive API to lookup our GSheet by name and get its GID
    for handling with the Sheets API. Unfortunately, this means we need
    to authenticate two different APIs with Google and as our app isn't
    signed yet, we need to approve this access twice every week.
    
    As we're always targetting the same Google Sheet anyway, let's instead
    use the hard-coded sheet ID from the config JSON and skip the Drive
    API call entirely.

--- src/bin/player_uploader.rs
+++ src/bin/player_uploader.rs
@@ -1,7 +1,7 @@
 mod fm_data;
 
 use fm_data::google_api::{
-    clear_sheet_area, google_drive_hub, google_sheets_hub, sheet_to_id, upload_attributes,
+    clear_sheet_area, google_sheets_hub, upload_attributes,
     GSheetsHub,
 };
 use fm_data::config::Configuration;
@@ -37,13 +37,14 @@ fn update_google_sheets(hub: &GSheetsHub, sheet_id: &str, opts: &Configuration) 
         "A2:AX58",
         &opts.input.data_html
     );
-    html_to_gsheet(
+    /*html_to_gsheet(
         &hub,
         sheet_id,
         &opts.google.team_perf_sheet,
         "A2:AW58",
         &opts.input.team_perf_html
-    );
+    );*/
+    /*
     html_to_gsheet(
         &hub,
         sheet_id,
@@ -51,10 +52,13 @@ fn update_google_sheets(hub: &GSheetsHub, sheet_id: &str, opts: &Configuration) 
         "A2:AU400",
         &opts.input.league_perf_html
     );
+    */
 }
 
 fn do_update_google(opts: &Configuration) {
 
+    /* Removed: we always use the same sheet ID and this call will always require another authentication for the Drive API,
+       which is annoying. Hence, rather hard-code the sheet ID in the config.
     /* Step 1: We need the sheet ID for our spreadsheet and we get that from Google Drive. */
     let gdh = google_drive_hub(
         &opts.google.creds_file,
@@ -66,20 +70,20 @@ fn do_update_google(opts: &Configuration) {
         &opts.google.spreadsheet_name,
         sheet_id
     );
+    */
 
     /* Step 2: Now we can use the sheet ID to clear the target range and upload new data. */
     let hub = google_sheets_hub(
         &opts.google.creds_file,
         &opts.google.token_file);
 
-    update_google_sheets(&hub, sheet_id.as_str(), &opts);
+    update_google_sheets(&hub, &opts.google.spreadsheet_name, &opts);
 }
 
 fn main() {
     let start_time = Instant::now();
 
     let config = fm_data::config::read_configuration().unwrap();
-    println!("{:?}", config);
 
     do_update_google(&config);
     println!(
